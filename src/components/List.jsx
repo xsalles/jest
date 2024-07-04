2d24bf3812b017b7141506c980072dcee733dcdc
xsalles <xs.salles@gmail.com>
2024-07-04 13:57:19 -0300

    feat: used findyByText and queryBy was used to verify the absence of element.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
 
+export default function List() {
+  const [list, setList] = useState(["Albert", "Elon", "Palmer"]);
 
-export default function List (){
-    return(
-        <h1>Sales</h1>
-    )
+  function addToList() {
+    setList((state) => [...state, "Halland"]);
+  } //->  With the "getByText"
+
+  // function addToList () {
+  //         const timer = setTimeout(() => {
+  //             setList(state => [...state, "Halland"])
+  //         }, 1000)
+  //         return () => clearTimeout(timer); //-> Clean to avoid memory leak
+  //      // With the findByText
+  // }
+
+  function removeFromList() {
+    setList((state) => state.filter((item) => item != item));
+  }
+
+  return (
+    <>
+      <button onClick={addToList}>Add</button>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item}>
+            {item}
+            <button onClick={() => removeFromList(item)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- src/components/List.spec.jsx
+++ src/components/List.spec.jsx
@@ -1,9 +1,55 @@
-import { render, screen } from "@testing-library/react"
-import "@testing-library/jest-dom"
-import List from "../components/List"
+import {
+  queryByText,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import List from "../components/List";
+import userEvent from "@testing-library/user-event";
 
-test('Jest', () => {
-    const { getByText } = render(<List/>)
+describe("List Component", () => {
+  it("should show the elements in page", () => {
+    const { getByText } = render(<List />);
 
-    expect(screen.getByText('Sales')).toBeInTheDocument(); 
-})
+    expect(getByText("Albert")).toBeInTheDocument();
+    expect(getByText("Elon")).toBeInTheDocument();
+    expect(getByText("Palmer")).toBeInTheDocument();
+  });
+
+  it("should be visible element on click in button", async () => {
+    const { getByText, findByText, debug } = render(<List />);
+
+
+    const addButton = screen.getByText("Add");
+      
+    debug();
+
+    userEvent.click(addButton);
+
+    debug();
+
+     await waitFor(() => {
+       expect(getByText("Halland")).toBeInTheDocument();
+     } )
+
+    // const element = await screen.findByText("Halland")
+
+    // expect(element).toBeInTheDocument(); -> With findByText
+  });
+
+  it("should be visible element on click in button", async () => {
+    const { getAllByText, queryByText } = render(<List />);
+
+
+    const removeButtons = getAllByText("Remove");
+      
+    userEvent.click(removeButtons[0]);
+
+     await waitFor(() => {
+       expect(queryByText("Albert")).not.toBeInTheDocument();
+     } )
+
+    })
+});
